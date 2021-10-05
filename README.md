4b0e228d75aac6d5949644d7cd4dc78539969d95
justnatsu <zainfalah26@gmail.com>
2021-10-05 15:17:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-7mkdbd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-7mkdbd)
---
+++ index.ts
@@ -0,0 +1,14 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let N1, N2, N3: number;
+N1 = parseInt(prompt('inter number'));
+N2 = parseInt(prompt('inter number'));
+N3 = N2 * N1;
+console.log(N3);
+if (N3 >= 0) {
+  console.log('positiv number');
+} else {
+  console.log('negative number');
+}
